f1975c33c7c93eed93747d32a2a7331214099b70
CorySonderman <sonderman2000@yahoo.com>
2023-10-07 15:35:38 -0500

    Completed exercise

--- Classes/Program.cs
+++ Classes/Program.cs
@@ -1,10 +1,19 @@
-﻿namespace Classes
+﻿using Microsoft.VisualBasic;
+
+namespace Classes
 {
     public class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            Car car1 = new Car();
+            car1.Year = 2007;
+            car1.Make = "Koenigsegg";
+            car1.Model = "CCXR";
+
+            Console.Write($"{car1.Year} {car1.Make} {car1.Model}");
+            Console.WriteLine();
         }
+        
     }
 }
